7b46a9c0325b5342036bbcbbf0c656ace25d97cd
Tiago Kieliger <tiago.kieliger@gmail.com>
2018-03-02 22:02:06 +0800

    add some helper functions to perform the search operation

--- HW #2/search.py
+++ HW #2/search.py
@@ -3,25 +3,21 @@ import re
 import nltk
 import sys
 import getopt
-
+from tools import *
 """ 
 Shunting_yard algorithm that parses boolean queries. 
 Based on pseudo code given at https://en.wikipedia.org/wiki/Shunting-yard_algorithm
 """
 
-#operators_precedence = {'OR': 1, 'AND': 2, 'OR': 3, '(': 4, ')': 4}
-operators_precedence = {'-': 2, '+': 2, '/': 3, 'x': 3, '^': 4, '(': 5, ')': 5}
-
-def test():
-    print("hi")
 def shunting_yard(query):
+    operators_precedence = {'OR': 1, 'AND': 2, 'OR': 3, '(': 4, ')': 4}
+    # operators_precedence = {'-': 2, '+': 2, '/': 3, 'x': 3, '^': 4, '(': 5, ')': 5}
     output = []
     operator_stack = []
     for token in query:
-        print(operator_stack)
-        if not is_operand(token) and not is_bracket(token):
+        if not is_operator(token) and not is_bracket(token):
             output.append(token)
-        if is_operand(token):
+        if is_operator(token):
             while not len(operator_stack) == 0 and ((operators_precedence[last(operator_stack)] > operators_precedence[
                     token] or (operators_precedence[last(operator_stack)] == operators_precedence[
                     token] and is_left_associative(token))) and not last(operator_stack) == '('):
@@ -37,20 +33,85 @@ def shunting_yard(query):
         output.append(operator_stack.pop())
     return output
 
+
 def last(stack):
     length = len(stack)
     return stack[length - 1]
 
-def is_operand(token):
-    #return token == 'AND' or token == 'OR' or token == 'NOT'
-    return token == '-' or token == '+' or token == '/' or token == '^' or token == 'x'
+def is_operator(token):
+    return token == 'AND' or token == 'OR' or token == 'NOT'
+    #return token == '-' or token == '+' or token == '/' or token == '^' or token == 'x'
 
 def is_bracket(token):
     return token == '(' or token == ')'
 
 def is_left_associative(token):
-    #return token == 'AND' or token == 'OR'
-    return token == '-' or token == '+' or token == '/' or token == 'x'
+    return token == 'AND' or token == 'OR'
+    #return token == '-' or token == '+' or token == '/' or token == 'x'
+
+"""""
+    Transforms a post-fix boolean expression with raw tokens as operands into an expression with the tokens
+    replaced by the corresponding posting lists
+"""""
+
+def prepare_query(query):
+    output = []
+    for term in query:
+        if is_operator(term):
+            output.append(term)
+        else:
+            token = tools.tokenize(term)
+            output.append(dictionnary(token))
+    return output
+
+"""""
+    Reads the file "queries_file_name" which contains a query by line and transform it in a list of queries where
+    the tokens are posting lists
+"""""
+def prepare_queries(queries_file_name):
+    output = []
+    queries = open(queries_file_name, "r")
+    for line in queries:
+        output.append(prepare_query(line))
+    close(queries)
+    return output
+
+"""""
+    Evaluates a post-fix expression where the operands are roots of linked lists and the operators are boolean
+"""""
+def evaluate(query):
+    stack = []
+    for token in query:
+        if is_operator(token):
+            if token == 'AND' :
+                result = and_op(stack.pop(), stack.pop())
+            if token == 'OR' :
+                result = or_op(stack.pop())
+            if token == 'NOT' :
+                result = not_op(stack.pop())
+            stack.append(result)
+        else:
+            stack.append(token)
+    return stack.pop()
+
+def and_op(postings1, postings2):
+    #TODO
+    return None
+
+def not_op(postings):
+    #TODO
+    return None
+
+def or_op(postings1, postings2):
+    #TODO
+    return None
+
+def search(dictionnary, postings_file_name, queries, file_of_output_name):
+    output = open(file_of_output_name, "w")
+    for query in queries:
+        output.write(evaluate(query))
+    close(output)
+
 """""
 def usage():
     print
