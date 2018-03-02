d9ae12caeb02776818cb3a44d023c57339d2735a
jheitmann <julien.heitmann@epfl.ch>
2018-03-02 22:09:34 +0800

    Index with cpickle.

--- HW #2/index.py
+++ HW #2/index.py
@@ -4,6 +4,7 @@ import nltk
 import sys
 import getopt
 import linecache
+import tempfile
 try:
    import cPickle as pickle
 except:
@@ -42,9 +43,9 @@ NUMBER_OF_DOCS = 10 # Number of documents in the directory directory-of-document
 index = {} # Inverted index
 vocab_size = 0
 
-with open(output_file_dictionary, 'w') as outfile_dict, open(output_file_postings, 'r+', encoding='utf-8') as outfile_post:
+with open(output_file_dictionary, 'w') as outfile_dict, open(output_file_postings, 'r+') as outfile_post: # Problem with no such file
 	# Create the index (Part 1)
-	for doc in range(1, number_of_docs+1):
+	for doc in range(1, NUMBER_OF_DOCS+1):
 		doc_path = input_directory + str(doc)
 		with open(doc_path, 'r') as next_doc:
 			for line in next_doc:
@@ -56,7 +57,7 @@ with open(output_file_dictionary, 'w') as outfile_dict, open(output_file_posting
 						new_line = pickle.dumps(Root(doc)) + '\n' # Serializes a new root, containing doc
 						outfile_post.seek(0,2) # 2: we want to append the new words posting list
 						outfile_post.write(new_line)
-						index[tokenized_word][1] += 1 # First occurence 
+						index[tokenized_word] = (index[tokenized_word][0], index[tokenized_word][1]+1, index[tokenized_word][2]) # First occurence, change this
 						vocab_size += 1 
 					else:
 						"""
@@ -65,19 +66,27 @@ with open(output_file_dictionary, 'w') as outfile_dict, open(output_file_posting
 						posting_list
 						"""
 						line_number = index[tokenized_word][0]
-						posting_list = pickle.loads(linecache.getline(outfile_post, line_number)) # Cache -> clear
+						posting_list = pickle.loads(linecache.getline(output_file_postings,line_number)) # Cache -> clear, -1 : remove newline [:-1]
 						if posting_list.last.docID != doc:
 							posting_list.append_element(Element(doc))
 							new_line = pickle.dumps(posting_list) + '\n'
 							# Need to overwrite the whole file
-							index[tokenized_word][1] += 1
+							output_file.seek(0)
+							fp = tempfile.TemporaryFile() # temporay file
+							fp.write(new_line)
+							offset = 0
+							for i, next_posting_list in enumerate(outfile_post):
+								if i < line_number:
+									offset += len(next_posting_list)
+								elif i > line_number:
+									fp.write(next_posting_list)
+
+							fp.seek(0)
+							outfile_post.seek(offset) 
+							for pl in fp:
+								outfile_post.write(pl)
+							fp.close()
+
+							index[tokenized_word] = (index[tokenized_word][0], index[tokenized_word][1]+1, index[tokenized_word][2]) # Change this
+	linecache.clearcache()
 	# Add Skip pointers (Part 2)
-
-
-
-
-						
-					
-
-
-
--- HW #2/tools.py
+++ HW #2/tools.py
@@ -7,15 +7,20 @@ import getopt
 def tokenize(word_to_process): 
 	return word_to_process
 
-class Root(object)
-    def __init__(self, element):
-        self.head = element
-        self.last = element
+class Root(object):
+    def __init__(self, docID):
+        self.head = Element(docID)
+        self.last = self.head
         return
 
     def append_element(self, element):
         self.last.add_next(element)
         self.last = element
+        return
+
+    def insert_after(self, predecessor, element): # Needs to be implemented
+        return 
+
 
 
 class Element(object):
