35caebb1f96c6128105e25e2c2a817bc16651231
jheitmann <julien.heitmann@epfl.ch>
2018-03-03 21:25:12 +0800

    Correct docID and skip.

--- HW #2/tools.py
+++ HW #2/tools.py
@@ -19,27 +19,6 @@ def pack_bytes(n, nbr_chars):
 def unpack_string(s):
     return int(s,16) # explain 16
 
-"""
-Packs integer n into its hexadecimal representation string,
-of length nbr_bytes (Big Endian format)
-"""
-def pack_bytes_old(n, nbr_bytes):
-    s = ""
-    while n:
-        s = chr(n % 0x100) + s
-        n = n / 0x100
-    s = s.rjust(nbr_bytes, "\x00")
-    return s
-
-def unpack_string(s, nbr_bytes): # nbr_bytes = len(s)
-    n = 0
-    weight = 1
-    for i in range(nbr_bytes):
-        n += ord(s[-1]) * weight
-        s = s[:-1]
-        weight = weight * 0xff
-    return n
-
 def new_document(flag, docID):
     return pack_bytes(flag,1) + pack_bytes(docID,8)
 
@@ -55,32 +34,3 @@ class BidirectionalDict(dict):
     def __delitem__(self, key):
         dict.__delitem__(self, self[key])
         dict.__delitem__(self, key)
-
-class Root(object):
-    def __init__(self, docID):
-        self.head = Element(docID)
-        self.last = self.head
-        return
-
-    def append_element(self, element):
-        self.last.add_next(element)
-        self.last = element
-        return
-
-    def insert_after(self, predecessor, element): # Needs to be implemented
-        return 
-
-class Element(object):
-    def __init__(self, docID):
-        self.docID = docID
-        return
-
-    def add_next(self, element):
-        self.next = element
-        return
-
-    def add_skip_pointer(self, element):
-	    self.skip_pointer = element
-	    return
-
-    
