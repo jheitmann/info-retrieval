c61e04e7e6d7e6043a2d001425c614689ae0b333
Tiago Kieliger <tiago.kieliger@gmail.com>
2018-03-04 14:36:02 +0800

    Comment most of search.py file

--- HW #2/search.py
+++ HW #2/search.py
@@ -10,12 +10,16 @@ except:
    import pickle
 
 """ 
-Shunting_yard algorithm that parses boolean queries. 
-Based on pseudo code given at https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+    Shunting_yard algorithm that parses boolean expressions in in-fix mode to post-fix (reverse polish notation). 
+    Based on the pseudo code given at https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+    
+    - The expressions can only use the following operators : 'OR', 'AND', 'NOT', '(', ')'
+    - Operands and operators must be separated by a space
+    
+    Example of a correct expression : 'windows and ( xp or vista )'
 """
-
 def shunting_yard(query):
-    operators_precedence = {'OR': 1, 'AND': 2, 'OR': 3, 'NOT': 4, '(': 5, ')': 5}
+    operators_precedence = {'OR': 1, 'AND': 2, 'NOT': 3, '(': 4, ')': 4}
     output = []
     operator_stack = []
     for token in query:
@@ -37,38 +41,72 @@ def shunting_yard(query):
         output.append(operator_stack.pop())
     return output
 
-
+""" 
+    Returns the last element of a stack (python list)
+"""
 def last(stack):
     length = len(stack)
     return stack[length - 1]
 
+""" 
+    Returns whether the given token (python string) is an operator
+"""
 def is_operator(token):
     return token == 'AND' or token == 'OR' or token == 'NOT'
 
+""" 
+    Returns whether the given token (python string) is a bracket
+"""
 def is_bracket(token):
     return token == '(' or token == ')'
 
+"""
+    Return whether the given token (python string) is a left associative operator (NOT operator is right associative)
+"""
 def is_left_associative(token):
     return token == 'AND' or token == 'OR'
 
-
-
 """""
-    Reads the file "queries_file_name" which contains a query by line and transform it in a list of queries where
-    the tokens are posting lists
+    Reads the file "queries_file_name" which contains one query at each line and outputs a list containing
+    the query in post-fix notation, where the terms have been stemmed and case-folded and then replaced
+    by their corresponding posting-list as found from the inverted index.
 """""
-def prepare_queries(queries_file_name, postings_file_name, dictionary):
-    output = []
-    queries = open(queries_file_name, "r")
+def prepare_queries(queries_file_name, postings_file_name, dictionary_file_name):
+    # output list of queries
+    queries = []
+
+    raw_queries = open(queries_file_name, "r")
     postings = open(postings_file_name, "r")
+
+    # the dictionary is loaded from file using the pickle library
+    serialized_dictionary = open(dictionary_file_name, "r")
+    dictionary = pickle.load(serialized_dictionary)
+    serialized_dictionary.close()
+
+    # the postings cache avoids retrieving the same postings list from disk (costly) multiple times
     postings_cache = {}
-    for line in queries.read().splitlines():
+
+    # each query in the queries_file is traversed
+    for line in raw_queries.read().splitlines():
         prepared_query = []
-        splitted = line.split(' ')
-        for term in splitted:
-            if is_operator(term):
-                prepared_query.append(term)
+        split = line.split(' ')
+
+        # each word in the query is traversed, a word can either be a term (e.g 'bill') or an operator (e.g 'AND')
+        for word in split:
+
+            # if the word is an operator, we leave it as it is.
+            if is_operator(word):
+                operator = word
+                prepared_query.append(operator)
+
+            # if the word is a term, we need to reduce it (stemming, case-folding, etc)
+            # and replace it by the corresponding posting list
             else:
+                term = word
+
+                # we need to separate terms and parenthesis (=add space).
+                # for example 'windows and (xp or vista)' should be replaced by 'windows and ( xp or vista )'
+                # this is needed to ease the work of the shunting-yard algorithm
                 if(term[0] == '('):
                     term = term[1:len(term)]
                     prepared_query.append('(')
@@ -76,27 +114,50 @@ def prepare_queries(queries_file_name, postings_file_name, dictionary):
                 if(term[len(term)-1] == ')'):
                     term = term[0:len(term)-1]
                     right_parenthesis = True
-                token = stem_and_casefold(term)
-                offset = dictionary[token][1]
-                # this posting list has not already been read from disk thus we cache it for efficiency
-                if not offset in postings_cache:
-                    postings.seek(offset)
-                    line = postings.readline()
-                    line = line[:-1] #TODO DO MANUALLY
-                    postings_cache[offset] = posting_list(line)
-                posting = postings_cache[offset]
+
+                # the term is reduced in the same way as in the indexing part
+                term = stem_and_casefold(term)
+
+                # the position in the file of the posting-list corresponding to a term is given by the
+                # offset (= pointer) stored in the dictionary (= inverted index)
+                if term in dictionary:
+                    offset = dictionary[term][1]
+
+                    # this posting list has not already been read from disk thus we cache it for efficiency
+                    if offset is not None and not offset in postings_cache:  # TODO offset not in instead of not offset in
+                        postings.seek(offset)
+                        line = postings.readline()
+                        line = line[:-1]  # TODO DO MANUALLY
+                        postings_cache[offset] = posting_list(line)
+
+                    posting = postings_cache[offset]
+
+                # if the term is not in the training corpus the corresponding posting-list is empty
+                else:
+                    postings = posting_list([])
+
+                # we append the term to the query
                 prepared_query.append(posting)
+
+                # if there is a right parenthesis we separate it from the term (= add space)
                 if right_parenthesis:
                     prepared_query.append(")")
-                #print(token+" : " + str(posting.to_string()))
+
+        # we perform the shunting-yard algorithm on the query to transform it from an
+        # in-fix expression to a post-fix expression
         post_fixed_prepared_query = shunting_yard(prepared_query)
-        output.append(post_fixed_prepared_query)
-    queries.close()
+
+        # the query is appended to the list of queries
+        queries.append(post_fixed_prepared_query)
+
+    raw_queries.close()
     postings.close()
-    return output
+
+    return queries
 
 """""
     Evaluates a post-fix expression where the operands are roots of linked lists and the operators are boolean
+    The pseudo code algorithm is given at https://en.wikipedia.org/wiki/Reverse_Polish_notation
 """""
 def evaluate(query, corpus):
     stack = []
@@ -105,21 +166,30 @@ def evaluate(query, corpus):
             if token == 'AND' :
                 result = and_op(stack.pop(), stack.pop())
             if token == 'OR' :
-                result = or_op(stack.pop())
+                result = or_op(stack.pop(), stack.pop())
             if token == 'NOT' :
-                result = not_op(stack.pop(), corpus)
+                result = not_op(stack.pop(), posting_list(corpus))
             stack.append(result)
         else:
             stack.append(token)
     return stack.pop()
 
+"""""
+    AND operation between 2 posting lists encapsulated in the custom class "posting_list"
+    the algorithm follows the idea of your typical linked-list merge algorithm
+"""""
 def and_op_skip(postings1, postings2):
     result = ""
+    postings1.rewind()
+    postings2.rewind()
     element1 = postings1.next()
     element2 = postings2.next()
 
+    # as long as we did not reach the end a both lists
     while element1 is not None and element2 is not None:
 
+        # if the element1 is smaller than element2 we either replace element1 by the next one in the list or
+        # replace it by the one pointed by the skip pointer if is possible
         if element1 is not None and element1 < element2:
             skip_pointer = postings1.skip_pointer()
             if skip_pointer:
@@ -132,6 +202,9 @@ def and_op_skip(postings1, postings2):
             else:
                  element1 = postings1.next()
 
+        # symmetric case of the previous one :
+        # if the element2 is smaller than element1 we either replace element2 by the next one in the list or
+        # replace it by the one pointed by the skip pointer if is possible
         elif element2 is not None and element1 > element2:
             skip_pointer = postings2.skip_pointer()
             if skip_pointer:
@@ -144,13 +217,15 @@ def and_op_skip(postings1, postings2):
             else:
                 element2 = postings2.next()
 
+        # in this case both elements are the same, hence this element has to be added to the result
         else:
-            result += new_document(0, element1)
+            result += new_node(0, element1)
             element1 = postings1.next()
             element2 = postings2.next()
 
     return posting_list(result)
 
+#TODO delete this method and replace it by the one that uses skips pointers
 def and_op(postings1, postings2):
     result = ""
     postings1.rewind()
@@ -164,40 +239,60 @@ def and_op(postings1, postings2):
         elif element2 is not None and element1 > element2:
             element2 = postings2.next()
         else:
-            result += new_document(0, element1)
+            result += new_node(0, element1)
             element1 = postings1.next()
             element2 = postings2.next()
 
     return posting_list(result)
 
+"""""
+    NOT operation on 1 posting list encapsulated in the custom class "posting_list"
+    the algorithm follows the idea of your typical linked-list merge algorithm using
+    the corpus (posting-list containing every document). Corpus is a posting-list
+    containing every term from the data set
+"""""
 def not_op(postings1, corpus):
     # we assume that postings1 is a subset of corpus
     result = ""
     postings1.rewind()
-    postings2 = posting_list(corpus)
+    postings2 = corpus
+    postings2.rewind
+
+    # if postings1 is empty, then its inverse is the whole corpus
+    if element1 is None:
+        return postings2
 
     element1 = postings1.next()
     element2 = postings2.next()
 
-    if element1 is None:
-        return postings2
-
+    # as long as we haven't reached the end of both lists
     while element1 is not None or element2 is not None:
 
+        # since the corpus contains every element from postings1 the case where
+        # element1 < element2 is not possible
         if element2 is None or (element1 is not None and element1 < element2):
             print("Error this case shouldn't happen")
             element1 = postings1.next()
 
+        # if element2 < element1 then it means that element2 appears in the corpus
+        # and not in the postings1, hence it is added to the output and we jump to
+        # the next element
         elif element1 is None or (element2 is not None and element1 > element2):
-            result += new_document(0, element2)
+            result += new_node(0, element2)
             element2 = postings2.next()
 
+        # in this case both elements are the same, hence they are not added to the output
+        # and we jump the the next element of each list
         else:
             element1 = postings1.next()
             element2 = postings2.next()
 
     return posting_list(result)
 
+"""""
+    OR operation between 2 posting lists encapsulated in the custom class "posting_list"
+    the algorithm follows the idea of your typical linked-list merge algorithm
+"""""
 def or_op(postings1, postings2):
     result = ""
     postings1.rewind()
@@ -205,29 +300,36 @@ def or_op(postings1, postings2):
     element1 = postings1.next()
     element2 = postings2.next()
 
+    # if the first posting list is empty, we can output the second one
     if element1 is None:
         return postings2
+
+    # symmetrically, if the second posting list is empty, we can output the first one
     if element2 is None:
         return postings1
 
+    # as long as we haven't reached the end of both lists we go through both lists and output every element
+    # appearing in any of the list once
     while element1 is not None or element2 is not None:
 
         if element2 is None or (element1 is not None and element1 < element2):
-            result += new_document(0, element1)
+            result += new_node(0, element1)
             element1 = postings1.next()
 
         elif element1 is None or (element2 is not None and element1 > element2):
-            result += new_document(0, element2)
+            result += new_node(0, element2)
             element2 = postings2.next()
 
         else:
-            result += new_document(0, element1)
+            result += new_node(0, element1)
             element1 = postings1.next()
             element2 = postings2.next()
 
     return posting_list(result)
 
-
+"""""
+    This class encapsulates a raw posting-list and offers a convenient interface to interact with the posting-list
+"""""
 class posting_list(object):
     pointer = 0
     list = []
@@ -270,19 +372,18 @@ class posting_list(object):
                 result += " "
         return result
 
-# term -> (doc_freq, offset_in_bytes)
-def prepare_dictionary(dictionary_file_name):
-    file = open(dictionary_file_name, "r")
-    dictionary = pickle.load(file)
-    return dictionary
-
 def search(dictionary_file_name, postings_file_name, queries_file_name, file_of_output_name):
-    dictionary = prepare_dictionary(dictionary_file_name)
-    queries = prepare_queries(queries_file_name, postings_file_name, dictionary)
+    queries = prepare_queries(queries_file_name, postings_file_name, dictionary_file_name)
     output = open(file_of_output_name, "w")
 
+    #EXPLAIN THIS DODO
+    postings = open(postings_file_name, "r")
+    postings.seek(dictionary[DOC_LIST][1])
+    corpus = postings.readline()
+    corpus = corpus[:-1]  # TODO DO MANUALLY
+    postings.close()
     for query in queries:
-        output.write(str(evaluate(query, dictionary[DOC_LIST]).to_string())+"\n")
+        output.write(str(evaluate(query, corpus).to_string())+"\n")
     output.close()
 
 
