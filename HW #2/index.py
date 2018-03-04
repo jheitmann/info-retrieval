32ef642d48de07d20b715e6ff340e0703a75e13c
jheitmann <julien.heitmann@epfl.ch>
2018-03-04 14:31:19 +0800

    Cleaner Index code.

--- HW #2/index.py
+++ HW #2/index.py
@@ -5,7 +5,6 @@ import sys
 import getopt
 import linecache
 import tempfile
-import math
 try:
    import cPickle as pickle
 except:
@@ -15,24 +14,6 @@ from tools import *
 
 def usage():
     print "usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file"
-
-# Explain this method
-def replace_line(file, line_number, new_line):
-	with open(file, 'r+') as outfile:
-		fp = tempfile.TemporaryFile() 
-		fp.write(new_line)
-		offset = 0
-		for i, line in enumerate(outfile):
-			if i < line_number:
-				offset += len(line)
-			elif i > line_number:
-				fp.write(line)
-
-		fp.seek(0)
-		outfile.seek(offset) 
-		for pl in fp:
-			outfile.write(pl)
-		fp.close()
 
 
 input_directory = output_file_dictionary = output_file_postings = None
@@ -57,15 +38,17 @@ if input_directory == None or output_file_postings == None or output_file_dictio
     usage()
     sys.exit(2)
 
-documents = sorted(map(int, listdir(input_directory)[:50])) # 10 for testing purposes, save in txt file
+documents = sorted(map(int, listdir(input_directory)[:200])) # 50 for testing purposes
 
 dictionary = {} # Inverted index
-word_number = BidirectionalDict() # word --> number/line is a bijective mapping
-vocab_size = 0
+word_number = BidirectionalDict() # (word: number) is a bijective mapping
+vocab_size = 0 # Number of words encountered
 
-open(output_file_postings,'w').close() # Create empty file
+open(output_file_postings,'w').close() # Create an empty file
 
-#============================ Create the index (Part 1) ============================#
+#============================= Create the index (Part 1) =============================#
+"""
+"""
 for docID in documents: # Scan all the documents
 	doc_path = input_directory + str(docID) # Absolute path of the current document
 
@@ -75,56 +58,49 @@ for docID in documents: # Scan all the documents
 			words = nltk.word_tokenize(line) # Tokenized words
 
 			for word in words:
-				reduced_word = stem_and_casefold(word) # Change this
+				reduced_word = stem_and_casefold(word) 
 				dictionary.setdefault(reduced_word,0)
 
 				if dictionary[reduced_word] == 0: # First occurence of reduced_word
 					word_number[reduced_word] = vocab_size # Update word_number
-					#print "A word not seen: " + '(' + word + ')' + '\n' # Debug
-					new_line = new_document(0,docID) + '\n' # flag | docID (5 bytes), 1st one for debug: pack_bytes(vocab_size,4) + 
+					new_line = new_node(0,docID) + '\n' # flag | docID (5 bytes) 
 					
-					with open(output_file_postings, 'a') as outfile_post: # Append this new line
-						outfile_post.write(new_line)
+					with open(output_file_postings, 'a') as outfile_post: 
+						outfile_post.write(new_line) # Append this new line
 
 					dictionary[reduced_word] += 1
 					vocab_size += 1 
 
-				else: # Word already in index
-					#print "------- STATS -------\n"
-					#print "A word in doc nbr " + str(docID) + ": " + '(' + word + ')' + '\n' # Debug
-					line_number = word_number[reduced_word]
-					#print "Line number: " + str(line_number) + '\n' # Debug
+				else: # reduced_word already in index
+					line_number = word_number[reduced_word] + 1 # Using linecache, lines start at 1 (not at 0)
 					linecache.clearcache() # Explain this one
-					posting_list = linecache.getline(output_file_postings,line_number+1)[:-1] # '\n' ignored, explain +1
-					#print "Actual line vs line number: " + str(line_number) + " vs " + str(unpack_string(posting_list[:8])) + '\n'
-					#print "Length of posting list: " + str(len(posting_list)) + '\n' # Debug
-					#print "------- END STATS -------\n"
-					last_docID = unpack_string(posting_list[-4:])
+					posting_list = linecache.getline(output_file_postings,line_number)[:-1] # '\n' ignored
+					last_docID = unpack_string(posting_list[-ELEM_SIZE:])
 
-					if  last_docID != docID: # Explain args
-						new_line = posting_list + new_document(0,docID) + '\n' # Add auxiliary method?
+					if  docID != last_docID: # Explain args
+						new_line = posting_list + new_node(0,docID) + '\n' 
 						replace_line(output_file_postings, line_number, new_line) # Need to overwrite the whole file
 						dictionary[reduced_word] += 1
 
 
-#=========================== Add Skip pointers (Part 2) ============================#
+#============================ Add Skip pointers (Part 2) =============================#
+"""
+"""
 fp = tempfile.TemporaryFile()
 
-#print "Vocabulary size: " + str(vocab_size) + '\n' # Debug
-
 with open(output_file_postings, 'r') as outfile_post:
-	for i, next_posting_list in enumerate(outfile_post):
-		#print "Ith word: " + str(i) + '\n' # Debug
-		reduced_word = word_number[i]
-		doc_frequency = dictionary[reduced_word]
-		floor = int(math.floor(math.sqrt((len(next_posting_list)-1)/9))) # -1: '\n', 9 = length of docID
-		nbr_skip_ptr = floor-1  # Explain this value
-		length_skip_ptr = floor+1 # No skip pointers overlap, hence l * 9chars(1: flag | 8: docID)
+
+	for next_posting_list in outfile_post:
+		skip_val = skip_value(len(next_posting_list[:-1])) # '\n' ignored
+		nbr_skip_ptr = skip_val-1  # Explain this value
+		skip_ptr_length = skip_val+1 # No skip pointers overlap
+
 		new_line = next_posting_list
-		for j in range(nbr_skip_ptr):
-			ptr_addr = 9*(j*length_skip_ptr) + 8*j # Explain this formula
-			ptr = ptr_addr+9*(length_skip_ptr+1)-1 # Explain this formula
-			new_line = new_line[:ptr_addr] + new_document(1, unpack_string(new_line[ptr_addr:ptr_addr+9])) + new_pointer(ptr) + new_line[ptr_addr+9:] # Explain
+		for i in range(nbr_skip_ptr):
+			next_addr = node_addr(i,skip_ptr_length)
+			node_string = unpack_string(new_line[next_addr:(next_addr+NODE_SIZE)])
+			ptr = ptr_value(next_addr,skip_ptr_length) 
+			new_line = new_line[:next_addr] + new_node(1,node_string) + new_pointer(ptr) + new_line[(next_addr+NODE_SIZE):] # Explain
 		fp.write(new_line)
 
 fp.seek(0)
@@ -132,28 +108,30 @@ with open(output_file_postings, 'w') as outfile_post:
 	for next_posting_list in fp:
 		outfile_post.write(next_posting_list)
 fp.close()
+	
 
-
-#============================ Store the docIDs (Part 3) ============================#
-with open(output_file_postings, 'a') as outfile_post:
+#============================ Write index to file (Part 3) ===========================#
+"""
+"""
+with open(output_file_postings, 'r+') as outfile_post, open(output_file_dictionary, 'w') as outfile_dict:
+	# ---- Add a special word to the dictionary to get the list of all documents ---- #
 	word_number[DOC_LIST] = vocab_size
 
 	new_line = ""
 	for docID in documents:
-		new_line += new_document(0,docID)
+		new_line += new_node(0,docID)
 	new_line += '\n'
 
+	outfile_post.seek(0,2) # Append the list of all documents
 	outfile_post.write(new_line)
 	dictionary[DOC_LIST] = len(documents)
 	vocab_size += 1
 
-
-#========================== Write index to file (Part 4) ==========================#
-with open(output_file_postings, 'r') as outfile_post, open(output_file_dictionary, 'w') as outfile_dict: # Problem with no such file
+	# ---------------- Serialize the dictionary and save it in a file --------------- #
+	outfile_post.seek(0)
+	
 	offset = 0
-
 	for i, next_posting_list in enumerate(outfile_post):
-		#print "Ith word: " + str(i) + '\n' # Debug
 		reduced_word = word_number[i]
 		dictionary[reduced_word] = (dictionary[reduced_word], offset) 
 		offset += len(next_posting_list)
